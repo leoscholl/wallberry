90208eb246a47f8a9fba8fc5b2c7d927bb731eea
Leo <leo.scholl@gmail.com>
2018-07-16 19:41:28 -0700

    change start/stop behavior

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+pkill chromium
 sleep 30
 export DISPLAY=:0
 chromium-browser --kiosk --incognito http://localhost:5000 &
+sleep 30
+tvservice -p
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+tvservice -o
+pkill chromium
